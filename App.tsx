dcbece463c7f5274b5c4de32ccafc47f2c4d5fb3
Jacob Veilleux <jacob.veilleux@hotmail.com>
2022-02-02 10:09:38 -0500

    add graphcache

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { createClient, Provider as UrqlProvider } from 'urql';
+import {
+  createClient,
+  dedupExchange,
+  fetchExchange,
+  Provider as UrqlProvider,
+} from 'urql';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootNavigator } from './src/screens/Root.navigator';
+import { cacheExchange } from '@urql/exchange-graphcache';
+import schema from './src/graphql/graphql.schema.json';
 
 const client = createClient({
   url: 'http://localhost:3000/graphql',
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      schema: schema as any,
+      resolvers: {
+        Query: {
+          story: (_, args) => ({ __typename: 'Story', id: args.id }),
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 export default function App() {
